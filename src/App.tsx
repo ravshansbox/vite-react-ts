a1e16b079a5a47814bca8f386b83ff46557e9e26
Ravshan Samandarov <ravshansbox@gmail.com>
2025-04-07 09:13:41 +0300

    setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { ComponentType } from 'react';
 
 export const App: ComponentType = () => {
-  return <h1>Hello World!</h1>;
+  return <h1 className="text-3xl font-bold">Hello World!</h1>;
 };
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import viteReact from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [viteReact()],
+  plugins: [tailwindcss(), viteReact()],
 });
